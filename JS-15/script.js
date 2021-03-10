ff48eb7079509f92a10464d0b11053a6ed86964d
JoJovanna <75172430+JoJovanna@users.noreply.github.com>
2021-03-11 02:03:32 +0300

    Add files via upload

---
+++ JS-15/script.js
@@ -0,0 +1,21 @@
+$('.imPickle').hide();
+
+
+$('.Flip').on('click', function (){
+    $('.imPickle').slideDown(700);
+    $('.Flip').hide();
+    
+});
+
+$('.imPickle').on('click', function() {
+    $('.imPickle').hide();
+    $('.Flip').slideDown(500);
+
+})
+
+
+
+
+
+
+
